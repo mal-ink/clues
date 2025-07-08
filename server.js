3c0141ffe7c5b5755fac0692e816b4cdd2d9938f
Malini <malini.ayyappan09@gmail.com>
2025-07-08 10:58:41 -0700

    backedn:)

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-hi
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+const USERS_FILE = './users.json';
+
+app.use(cors());
+app.use(bodyParser.json());
+
+function loadUsers() {
+  try {
+    const data = fs.readFileSync(USERS_FILE);
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+}
+
+
+function saveUsers(users) {
+  fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+}
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const users = loadUsers();
+
+  const user = users.find(user => user.username === username && user.password === password);
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  res.json({ message: 'Login successful', username: user.username });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
