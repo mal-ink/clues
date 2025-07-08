27a26eb38dc06dd5f0d3af2b629a41d4f7e3277f
Malini <malini.ayyappan09@gmail.com>
2025-07-08 09:02:46 -0700

    🤷‍♀️

---
+++ start.js
@@ -0,0 +1,5 @@
+const loggedInUser = localStorage.getItem('loggedInUser');
+const userDropdown = document.getElementById('user-dropdown');
+const loginLink = document.getElementById('login-link');
+const logoutBtn = document.getElementById('logout-button');
+ 
